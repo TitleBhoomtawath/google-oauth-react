86b58666a35d5379c072a8df40367f20bcb425eb
TitleBhoomtawath <bhoomtawath.plinsut@foodpanda.com>
2022-01-26 10:46:06 +0700

    Use poc IAM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from "react-google-login";
 
 function App() {
+    const handleLogin = async googleData => {
+        console.log('token id', googleData.tokenId)
+        // const formData = new FormData()
+        // formData.append("code", googleData.tokenId)
+        const res = await fetch("http://localhost:9090/login-iam", {
+            method: "POST",
+            body: JSON.stringify({ token: googleData.tokenId }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json()
+        console.log('data', data)
+        // store returned user somehow
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+          clientId="318116953955-d74bskibjt28mlpmansr1i9v11avacki.apps.googleusercontent.com"
+          buttonText="Log in with Google"
+          onSuccess={handleLogin}
+          onFailure={handleLogin}
+          cookiePolicy={'single_host_origin'}
+
+      />
     </div>
   );
 }
